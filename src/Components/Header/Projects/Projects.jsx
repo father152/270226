89039680d9ef0e6d97ac8ce91ff5424f217cd1d3
Your Name <tsimbalenko.vv@gmail.com>
2026-03-16 09:23:42 +0200

    dfg

--- src/Components/Header/Projects/Projects.jsx
+++ src/Components/Header/Projects/Projects.jsx
@@ -6,8 +6,16 @@ import { Link } from 'react-router-dom'
 export default function Projects() {
   return (
     <div className="projects">
-        
-        <h1>Projects</h1>
+                <h1>Projects</h1>
+<div className="projects-container">
+
+  
+</div>
+
+
+
+
+
           <div className="back-home">
             <Link to="/"><img src={back} alt=''></img></Link>
         </div>
