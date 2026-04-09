103ba6e51fc76cc5516adac4bbbe8a815d90562d
Your Name <tsimbalenko.vv@gmail.com>
2026-04-09 15:16:25 +0300

    78878

--- src/Components/Header/Projects/Projects.jsx
+++ src/Components/Header/Projects/Projects.jsx
@@ -6,8 +6,7 @@ import "./projects.css"
 
 
 export default function Projects() {
-    
-  
+
   return (
     <div className="projects">
       <h1>Projects</h1>
@@ -15,20 +14,18 @@ export default function Projects() {
         <div className="title-projects">
           <ul>
             <Link to="/project1"><li>Project 1</li></Link>
-            <Link to="/project2"><li>Project 2</li></Link> 
-         
+            <Link to="/project2"><li>Project 2</li></Link>
+
           </ul>
         </div>
         <div className="object">
-         <h2>P R O J E C T S</h2>
+          <h2>P R O J E C T S</h2>
+          <p>React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro.React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro.
+            React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro.React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro.
+            React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro.React Flow is a MIT-licensed open source library. You can help us to ensure the further development and maintenance by subscribing to React Flow Pro. React Flow is a MIT-licensed open source library. You can help us to ensure.
+          </p>
         </div>
-
-
       </div>
-
-
-
-
 
       <div className="back-home">
         <Link to="/"><img src={back} alt=''></img></Link>
