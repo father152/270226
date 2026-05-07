5a6023d89d14a4baa791a80a7e944d9fd7f755c7
Your Name <tsimbalenko.vv@gmail.com>
2026-05-07 16:52:47 +0300

    455

--- src/Components/Header/Projects/Project1.jsx
+++ src/Components/Header/Projects/Project1.jsx
@@ -1,18 +1,23 @@
 import React from 'react'
 import back from "../../Image/free-icon-back-3183312.png"
 import { Link } from 'react-router-dom'
-
+import "./project1.css"
 
 
 export default function Project1() {
   return (
-    <div>
-        
-        
-        <h2>Project-1</h2>
+    <div className="project1">
 
 
- <div className="back-home">
+      <h2>Project-1</h2>
+      <div className="project1-container">
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam. Voluptas, voluptate. Doloribus, quisquam.</p>
+      </div>
+
+      <div className="back-home">
         <Link to="/projects">
           <img src={back} alt="home" />
         </Link>
--- src/Components/Header/Projects/Project2.jsx
+++ src/Components/Header/Projects/Project2.jsx
@@ -1,17 +1,17 @@
 import back from "../../Image/free-icon-back-3183312.png"
 import { Link } from 'react-router-dom'
-
-
+import "./project2.css"
+import photo1 from "../../Image/7858d44b402eff7aad9e5c34f8e77605.jpg"
 
 export default function Project2() {
   return (
-    <div>
-        
-        
-        <h2>Project-2</h2>
+    <div className="project2">
+      <h2>Project-2</h2>
+      <div className="project2-container">
+<img src={photo1} alt=""></img>
 
-
- <div className="back-home">
+      </div>
+      <div className="back">
         <Link to="/projects">
           <img src={back} alt="home" />
         </Link>
